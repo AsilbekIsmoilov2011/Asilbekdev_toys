a8a9b9405746dc5820b063cfc9d47a59bd0383df
unknown <fdr1names@gmail.com>
2025-01-31 08:55:27 +0500

    second

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 
 
 const menuBtn = document.querySelector('.menu-btn'),
-      navigation = document.querySelector(".nav-left__list");
+  headerCatalog = document.querySelector(".header-catalog");
+  body = document.body;
 
-      menuBtn.addEventListener('click', () => {
-        menuBtn.classList.toggle('active');
-        navigation.classList.toggle('active');
-      }); 
+
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('active');
+  headerCatalog.classList.toggle('active')
+
+  if (navigation.classList.contains('active')) {
+    body.style.overflow = 'hidden';
+  } else {
+    body.style.overflow = 'auto';
+  }
+});
+
+
+
+
+
+
